2d2d6f85a9654bb8b32b5679dd9d35da73d0f589
GabeEddyT <gtroy518@gmail.com>
2019-08-23 16:52:31 -0400

    Adjust ie-check to support NX
    Add `classList` polyfill, replace "let"s, 
    and move up the onload event handler assignment
    to (somewhat) support the 3DS browser engine.

--- ie-check.js
+++ ie-check.js
@@ -4,10 +4,22 @@
 try { 
     new URLSearchParams();
 } catch (error) {
+    // After the core-js polyfills are loaded, 
+    // attach babel-converted script
+    window.onload = function(){
+        const labScript = document.createElement("script");
+        labScript.src = "//gabeeddyt.gitlab.io/labs-2.0/labs-polyfill.min.js";                    
+        document.body.appendChild(labScript);
+    };
+
     console.log("You're using IE. Why are you like this?");
     
-    let corejs = document.body.appendChild(document.createElement("script"));
+    const corejs = document.body.appendChild(document.createElement("script"));
     corejs.src = "https://unpkg.com/core-js-bundle@3.2.1/index.js";
+
+    // Style.classList polyfill for NX
+    const classlist = document.body.appendChild(document.createElement("script"));
+    classlist.src = "https://cdnjs.cloudflare.com/ajax/libs/classlist/1.2.20171210/classList.min.js";
 
     // Mozilla's polyfill for `Element.closest`
     if (!Element.prototype.matches) {
@@ -34,20 +46,12 @@ try {
     
     // Trigger on changes to `data-offset`
     const observer = new MutationObserver(function(mutationsList, observer) {
-		for (let i = 0; i < mutationsList.length; i++) {	
-			let mutation = mutationsList[i];
+		for (var i = 0; i < mutationsList.length; i++) {	
+			const mutation = mutationsList[i];
 			/** @type {HTMLElement} */		
             const element = mutation.target;    
             // Insert value of `data-offset` where `var(--offset)` would be.
             offsetStyle.innerHTML = ".wrapper.hidden{ transform: scale(0.5) translate(0, " + element.getAttribute("data-offset") + ");}";
 		}
 	}).observe(document.getElementsByClassName("wrapper")[0], { attributes: true, attributeFilter: ["data-offset"] });
-    
-    // After the core-js polyfills are loaded, 
-    // attach babel-converted script
-    window.onload = function(){
-        let labScript = document.createElement("script");
-        labScript.src = "https://gabeeddyt.gitlab.io/labs-2.0/labs-polyfill.min.js";                    
-        document.body.appendChild(labScript);
-    };
 }
