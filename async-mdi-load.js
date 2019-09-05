4d358165127c7b5ab5da76e8545d7645c96d19f9
Gabe Troyan <gtroy518@gmail.com>
2019-09-05 01:41:34 -0400

    Load MDI asynchronously using a script
    So the page can load without being blocked

---
+++ async-mdi-load.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.media = "screen";
+    link.type = "text/css";
+    link.href = "https://cdn.materialdesignicons.com/3.7.95/css/materialdesignicons.min.css";
+    document.head.appendChild(link);
+});
